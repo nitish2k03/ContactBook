b20fecdc2863c3d506e207e65a7145c75bc9e183
Nitish <nitish.mahajanhalf8@gmail.com>
2024-06-13 17:13:17 +0530

    chore: Remove unnecessary close button in modal header

--- dashboard.php
+++ dashboard.php
@@ -159,7 +159,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title">Edit Contact</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                         </div>
                         <div class="modal-body">
                             <form method="post">
