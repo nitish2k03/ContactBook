a1fe3bd2c29793cad75c002b625824182afa26b1
Nitish <nitish.mahajanhalf8@gmail.com>
2024-06-13 23:01:00 +0530

    chore: Update login.php layout

--- login.php
+++ login.php
@@ -80,7 +80,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                   </form>
                 </div>
               </div>
-                
+              <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                </div>
+              </div>
             </div>
           </div>
         </div>
